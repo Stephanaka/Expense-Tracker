9871c0b50d6c81031b8f3302d1cb8fe9eaf0839c
Stefan Dimov <stefandimov21a@gmail.com>
2026-04-21 23:26:50 +0300

    Fix error variable name in MongoDB connection

--- EXPENSE-TRACKER/backend/config/db.js
+++ EXPENSE-TRACKER/backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URL, {});
         console.log("MongoDB connected");
     } catch (error) {
-        console.error("Error connecting to MongoDB", err);
+        console.error("Error connecting to MongoDB", error);
         process.exit(1);
     }
 };
