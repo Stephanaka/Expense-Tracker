e87815bb5c906c02833991823117b3e381008c12
Stefan Dimov <stefandimov21a@gmail.com>
2026-04-24 14:36:21 +0000

    Refactor upload image route to use controller
    Updated the upload image route to use the uploadImage controller for handling image uploads.

--- EXPENSE-TRACKER/backend/routes/authRoutes.js
+++ EXPENSE-TRACKER/backend/routes/authRoutes.js
@@ -1,29 +1,23 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
 
-// Defines user authentication routes, protected user data access, and image upload functionality.
-
 const {
     registerUser,
     loginUser,
     getUserInfo,
+    uploadImage, 
 } = require("../controllers/authController");
+
 const upload = require("../middleware/uploadMiddleware");
 
 const router = express.Router();
 
+// Auth routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/getUser", protect, getUserInfo);
 
-router.post("/upload-image", upload.single("image"), (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ message: "No file uploaded" });
-    }
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-    req.file.filename
-    }`;
-    res.status(200).json({ imageUrl });
-});
+// Upload Image - uses controller (Base64, no local storage)
+router.post("/upload-image", upload.single("image"), uploadImage);
 
 module.exports = router;
